614c2f70f57f729c0ef558456c928a7611000690
Tony Lyons <46404545+CreativeLyons@users.noreply.github.com>
2021-10-28 18:35:00 +0200

    Create 005.py

---
+++ Joint_Hotbox/W_hotbox/Single/Merge2/005.py
@@ -0,0 +1,12 @@
+#----------------------------------------------------------------------------------------------------------
+#
+# AUTOMATICALLY GENERATED FILE TO BE USED BY W_HOTBOX
+#
+# NAME: disjoint-over
+# COLOR: #5063c9
+# TEXTCOLOR: #ffffff
+#
+#----------------------------------------------------------------------------------------------------------
+
+for node in nuke.selectedNodes():
+    node.knob("operation").setValue("disjoint-over")
