6f379751d9705611b516de6dfc2925af0710632f
Tony Lyons <46404545+CreativeLyons@users.noreply.github.com>
2021-10-28 18:35:41 +0200

    Create 008.py

---
+++ Joint_Hotbox/W_hotbox/Single/Merge2/008.py
@@ -0,0 +1,11 @@
+#----------------------------------------------------------------------------------------------------------
+#
+# AUTOMATICALLY GENERATED FILE TO BE USED BY W_HOTBOX
+#
+# NAME: divide
+# COLOR: #4b5f4b
+#
+#----------------------------------------------------------------------------------------------------------
+
+for node in nuke.selectedNodes():
+    node.knob("operation").setValue("divide")
