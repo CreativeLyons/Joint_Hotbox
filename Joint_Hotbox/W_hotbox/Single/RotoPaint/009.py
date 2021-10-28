6b9fa7abfe863d80113cfb4f4f4a7f9de8406469
Tony Lyons <46404545+CreativeLyons@users.noreply.github.com>
2021-10-28 18:43:39 +0200

    Create 009.py

---
+++ Joint_Hotbox/W_hotbox/Single/RotoPaint/009.py
@@ -0,0 +1,15 @@
+#----------------------------------------------------------------------------------------------------------
+#
+# AUTOMATICALLY GENERATED FILE TO BE USED BY W_HOTBOX
+#
+# NAME: White Brush
+# COLOR: #ffffff
+# TEXTCOLOR: #000000
+#
+#----------------------------------------------------------------------------------------------------------
+
+for node in nuke.selectedNodes():
+    node.knob("toolbar_paint_color").setValue(1, 0)
+    node.knob("toolbar_paint_color").setValue(1, 1)
+    node.knob("toolbar_paint_color").setValue(1, 2)
+    node.knob("toolbar_paint_color").setValue(1, 3)
