f2b25b81d68c1afbb7531d317c6ae35ce7141d32
Tony Lyons <46404545+CreativeLyons@users.noreply.github.com>
2021-10-28 18:33:04 +0200

    Create 001.py

---
+++ Joint_Hotbox/W_hotbox/Single/Grade/001.py
@@ -0,0 +1,12 @@
+#----------------------------------------------------------------------------------------------------------
+#
+# AUTOMATICALLY GENERATED FILE TO BE USED BY W_HOTBOX
+#
+# NAME: rgb
+# COLOR: #516375
+# TEXTCOLOR: #ffffff
+#
+#----------------------------------------------------------------------------------------------------------
+
+for node in nuke.selectedNodes():
+    node.knob("channels").setValue("rgb")
