128609de5523303eb4eaf4227f464153a9b4fdd1
Tony Lyons <46404545+CreativeLyons@users.noreply.github.com>
2021-10-28 18:20:13 +0200

    Create 003.py

---
+++ Joint_Hotbox/W_hotbox/All/003.py
@@ -0,0 +1,12 @@
+#----------------------------------------------------------------------------------------------------------
+#
+# AUTOMATICALLY GENERATED FILE TO BE USED BY W_HOTBOX
+#
+# NAME: Align Horizontal --
+# COLOR: #37595b
+# TEXTCOLOR: #ffffff
+#
+#----------------------------------------------------------------------------------------------------------
+
+import Ym_alignNodes
+Ym_alignNodes.alignCY()
