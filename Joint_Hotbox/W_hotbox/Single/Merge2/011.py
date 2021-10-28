ed17527b49ea1e6803421d58b2504ba675f38e4b
Tony Lyons <46404545+CreativeLyons@users.noreply.github.com>
2021-10-28 18:36:20 +0200

    Create 011.py

---
+++ Joint_Hotbox/W_hotbox/Single/Merge2/011.py
@@ -0,0 +1,12 @@
+#----------------------------------------------------------------------------------------------------------
+#
+# AUTOMATICALLY GENERATED FILE TO BE USED BY W_HOTBOX
+#
+# NAME: BBox A
+# COLOR: #a377aa
+# TEXTCOLOR: #ffffff
+#
+#----------------------------------------------------------------------------------------------------------
+
+for node in nuke.selectedNodes():
+    node.knob("bbox").setValue("A")
