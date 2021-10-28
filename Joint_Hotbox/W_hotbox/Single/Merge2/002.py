66858b09ba93b75017e5237dbb3c1927048d14c6
Tony Lyons <46404545+CreativeLyons@users.noreply.github.com>
2021-10-28 18:34:25 +0200

    Create 002.py

---
+++ Joint_Hotbox/W_hotbox/Single/Merge2/002.py
@@ -0,0 +1,12 @@
+#----------------------------------------------------------------------------------------------------------
+#
+# AUTOMATICALLY GENERATED FILE TO BE USED BY W_HOTBOX
+#
+# NAME: screen
+# COLOR: #757575
+# TEXTCOLOR: #ffffff
+#
+#----------------------------------------------------------------------------------------------------------
+
+for node in nuke.selectedNodes():
+    node.knob("operation").setValue("screen")
