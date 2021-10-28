d33462eda6786b4c18a5315a45ce5aaf1e74431e
Tony Lyons <46404545+CreativeLyons@users.noreply.github.com>
2021-10-28 18:45:52 +0200

    Create 002.py

---
+++ Joint_Hotbox/W_hotbox/Single/Tracker4/002.py
@@ -0,0 +1,12 @@
+#----------------------------------------------------------------------------------------------------------
+#
+# AUTOMATICALLY GENERATED FILE TO BE USED BY W_HOTBOX
+#
+# NAME: RotoFromTrack
+# COLOR: #009e3f
+# TEXTCOLOR: #ffffff
+#
+#----------------------------------------------------------------------------------------------------------
+
+import rotoFromTrack
+rotoFromTrack.rotoFromTrack()
