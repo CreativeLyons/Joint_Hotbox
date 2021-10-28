3116f2caf58b0d6b1b9e919ae720313da473c07c
Tony Lyons <46404545+CreativeLyons@users.noreply.github.com>
2021-10-28 18:40:50 +0200

    Create 001.py

---
+++ Joint_Hotbox/W_hotbox/Single/RotoPaint/001.py
@@ -0,0 +1,12 @@
+#----------------------------------------------------------------------------------------------------------
+#
+# AUTOMATICALLY GENERATED FILE TO BE USED BY W_HOTBOX
+#
+# NAME: blur effect to 5
+# COLOR: #8e6044
+# TEXTCOLOR: #ffffff
+#
+#----------------------------------------------------------------------------------------------------------
+
+for node in nuke.selectedNodes():
+    node.knob("toolbar_effect_parameter1").setValue(5)
