e3ae9110c7a9c35704476d25efbd6e7da388942f
Tony Lyons <46404545+CreativeLyons@users.noreply.github.com>
2021-10-28 18:42:43 +0200

    Create 005.py

---
+++ Joint_Hotbox/W_hotbox/Single/RotoPaint/005.py
@@ -0,0 +1,12 @@
+#----------------------------------------------------------------------------------------------------------
+#
+# AUTOMATICALLY GENERATED FILE TO BE USED BY W_HOTBOX
+#
+# NAME: opacity to 1
+# COLOR: #eaeaea
+# TEXTCOLOR: #000000
+#
+#----------------------------------------------------------------------------------------------------------
+
+for node in nuke.selectedNodes():
+    node.knob("toolbar_opacity").setValue(1)
