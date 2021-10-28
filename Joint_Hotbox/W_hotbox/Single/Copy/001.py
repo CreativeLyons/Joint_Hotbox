8e55b2a2418d760390d254e05c2f568e7391722a
Tony Lyons <46404545+CreativeLyons@users.noreply.github.com>
2021-10-28 18:28:40 +0200

    Create 001.py

---
+++ Joint_Hotbox/W_hotbox/Single/Copy/001.py
@@ -0,0 +1,12 @@
+#----------------------------------------------------------------------------------------------------------
+#
+# AUTOMATICALLY GENERATED FILE TO BE USED BY W_HOTBOX
+#
+# NAME: Set to None
+# COLOR: #8e6b5e
+#
+#----------------------------------------------------------------------------------------------------------
+
+for n in nuke.selectedNodes():
+    n['from0'].setValue("none")
+    n['to0'].setValue("none")
