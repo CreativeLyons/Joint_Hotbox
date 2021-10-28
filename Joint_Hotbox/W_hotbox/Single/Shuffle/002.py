8cf4cec5abd2b0610cd26f731451554a2514e5b7
Tony Lyons <46404545+CreativeLyons@users.noreply.github.com>
2021-10-28 18:45:04 +0200

    Create 002.py

---
+++ Joint_Hotbox/W_hotbox/Single/Shuffle/002.py
@@ -0,0 +1,15 @@
+#----------------------------------------------------------------------------------------------------------
+#
+# AUTOMATICALLY GENERATED FILE TO BE USED BY W_HOTBOX
+#
+# NAME: Push RGBA to Out2
+# COLOR: #ad5a79
+#
+#----------------------------------------------------------------------------------------------------------
+
+n = nuke.selectedNode()
+
+n.knob('black').setValue('red')
+n.knob('white').setValue('green')
+n.knob('red2').setValue('blue')
+n.knob('green2').setValue('alpha')
