68b6816aedff0e23bbd29376d99e5e536d47625c
Tony Lyons <46404545+CreativeLyons@users.noreply.github.com>
2021-10-28 18:31:21 +0200

    Create 003.py

---
+++ Joint_Hotbox/W_hotbox/Single/DeepRead/003.py
@@ -0,0 +1,13 @@
+#----------------------------------------------------------------------------------------------------------
+#
+# AUTOMATICALLY GENERATED FILE TO BE USED BY W_HOTBOX
+#
+# NAME: set before/after/missing Black
+# COLOR: #262626
+#
+#----------------------------------------------------------------------------------------------------------
+
+for i in nuke.selectedNodes():
+    i["before"].setValue("black")
+    i["after"].setValue("black")
+    i["on_error"].setValue("black")
