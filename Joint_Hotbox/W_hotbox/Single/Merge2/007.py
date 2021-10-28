5fe46bab0af9361dcb4299859685a1a519f7ec1b
Tony Lyons <46404545+CreativeLyons@users.noreply.github.com>
2021-10-28 18:35:27 +0200

    Create 007.py

---
+++ Joint_Hotbox/W_hotbox/Single/Merge2/007.py
@@ -0,0 +1,12 @@
+#----------------------------------------------------------------------------------------------------------
+#
+# AUTOMATICALLY GENERATED FILE TO BE USED BY W_HOTBOX
+#
+# NAME: hypot
+# COLOR: #757575
+# TEXTCOLOR: #ffffff
+#
+#----------------------------------------------------------------------------------------------------------
+
+for node in nuke.selectedNodes():
+    node.knob("operation").setValue("hypot")
