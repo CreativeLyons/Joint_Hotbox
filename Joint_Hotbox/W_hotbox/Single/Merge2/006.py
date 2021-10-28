e78152f74c20d06ea4acf7478cfcfe5f61fe73f2
Tony Lyons <46404545+CreativeLyons@users.noreply.github.com>
2021-10-28 18:35:14 +0200

    Create 006.py

---
+++ Joint_Hotbox/W_hotbox/Single/Merge2/006.py
@@ -0,0 +1,11 @@
+#----------------------------------------------------------------------------------------------------------
+#
+# AUTOMATICALLY GENERATED FILE TO BE USED BY W_HOTBOX
+#
+# NAME: multiply
+# COLOR: #4b5f4b
+#
+#----------------------------------------------------------------------------------------------------------
+
+for node in nuke.selectedNodes():
+    node.knob("operation").setValue("multiply")
