41f4d61f0dd99700569c88d5db79ed4aa5363f42
Tony Lyons <46404545+CreativeLyons@users.noreply.github.com>
2021-10-28 18:33:24 +0200

    Create 003.py

---
+++ Joint_Hotbox/W_hotbox/Single/Grade/003.py
@@ -0,0 +1,12 @@
+#----------------------------------------------------------------------------------------------------------
+#
+# AUTOMATICALLY GENERATED FILE TO BE USED BY W_HOTBOX
+#
+# NAME: alpha
+# COLOR: #eaeaea
+# TEXTCOLOR: #000000
+#
+#----------------------------------------------------------------------------------------------------------
+
+for node in nuke.selectedNodes():
+    node.knob("channels").setValue("alpha")
