2f7d6ec361d51d497fef3c5e8315ba0f073f30e8
Tony Lyons <46404545+CreativeLyons@users.noreply.github.com>
2021-10-28 18:19:46 +0200

    Create 001.py

---
+++ Joint_Hotbox/W_hotbox/All/001.py
@@ -0,0 +1,12 @@
+#----------------------------------------------------------------------------------------------------------
+#
+# AUTOMATICALLY GENERATED FILE TO BE USED BY W_HOTBOX
+#
+# NAME: Align Vertically |
+# COLOR: #37595b
+# TEXTCOLOR: #ffffff
+#
+#----------------------------------------------------------------------------------------------------------
+
+import Ym_alignNodes
+Ym_alignNodes.alignCX()
