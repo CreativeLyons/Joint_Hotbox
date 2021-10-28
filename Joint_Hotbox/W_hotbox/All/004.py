1b3e66e1a2bae9d7e639d01d912155bc4069d4f1
Tony Lyons <46404545+CreativeLyons@users.noreply.github.com>
2021-10-28 18:20:25 +0200

    Create 004.py

---
+++ Joint_Hotbox/W_hotbox/All/004.py
@@ -0,0 +1,13 @@
+#----------------------------------------------------------------------------------------------------------
+#
+# AUTOMATICALLY GENERATED FILE TO BE USED BY W_HOTBOX
+#
+# NAME: Convert Gizmo to Group
+# COLOR: #d1d1d1
+# TEXTCOLOR: #000000
+#
+#----------------------------------------------------------------------------------------------------------
+
+import convertGizmosToGroups
+
+convertGizmosToGroups.convertGizmosToGroups()
