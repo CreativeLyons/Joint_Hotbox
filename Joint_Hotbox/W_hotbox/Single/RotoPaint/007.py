07b4e3b3198da3df869519f200017f9f72124200
Tony Lyons <46404545+CreativeLyons@users.noreply.github.com>
2021-10-28 18:43:10 +0200

    Create 007.py

---
+++ Joint_Hotbox/W_hotbox/Single/RotoPaint/007.py
@@ -0,0 +1,15 @@
+#----------------------------------------------------------------------------------------------------------
+#
+# AUTOMATICALLY GENERATED FILE TO BE USED BY W_HOTBOX
+#
+# NAME: Blue Brush
+# COLOR: #274aa8
+# TEXTCOLOR: #ffffff
+#
+#----------------------------------------------------------------------------------------------------------
+
+for node in nuke.selectedNodes():
+    node.knob("toolbar_paint_color").setValue(0, 0)
+    node.knob("toolbar_paint_color").setValue(0.2, 1)
+    node.knob("toolbar_paint_color").setValue(1, 2)
+    node.knob("toolbar_paint_color").setValue(1, 3)
